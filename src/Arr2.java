51c2ce0746004dab4c0b46f40e41c2fad24d4fb5
fw <fw777888@gmail.com>
2022-05-29 22:14:28 +0300

    22222

--- src/Arr2.java
+++ src/Arr2.java
@@ -4,7 +4,7 @@ public class Arr2 {
         array[0] = new int[1];
         array[1] = new int[2];
         array[2] = new int[3];
-        array[3] = new int[4 ];
+        array[3] = new int[5 ];
         int i, j, k = 0;
         for ( i = 0; i < array.length; i++) {
             for (j =0; j < array[i].length; j++) {
